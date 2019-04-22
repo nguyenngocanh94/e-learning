6f1836a2a6172d240385b98dd3e9e2d309aa6a0f
nguyen ngoc anh <20120035@student.hut.edu.vn>
2019-04-22 14:08:54 +0700

    add title:

--- student/views/layouts/main.php
+++ student/views/layouts/main.php
@@ -22,7 +22,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
     <link rel="shortcut icon" href="<?php HtmlHelper::getFavicon() ?>" type="image/x-icon" />
-    <title><?= Html::encode($this->title) ?></title>
+    <title>Hóa học trực tuyến</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <?php $this->head() ?>
