24c966fdd9257bf3a696b90bf61c1f51bc361dd6
nguyen ngoc anh <20120035@student.hut.edu.vn>
2019-04-22 16:22:03 +0700

    fix bug coure

--- sql/getAvailableCourse.sql
+++ sql/getAvailableCourse.sql
@@ -1,8 +1,8 @@
 select
-    if(enroll.student_id = :student_id, "enrolled", "available") is_enroll,
-    c.*, s.name subject, t.name teacher
+    c.name, s.name subject, t.name teacher, enroll.id, enroll.student_id, c.image1, c.description,
+    if(enroll.student_id = :student_id, "enrolled", "available") is_enroll
 from
     course c
         left join
-    enroll on enroll.course_id = c.id left join subject s on s.id = c.subject_id left join teacher t on t.id = c.teacher_id
-where if(:subject_id = 0, true, c.subject_id = :subject_id)
+    enroll on enroll.course_id = c.id and enroll.student_id = 3 left join subject s on s.id = c.subject_id left join teacher t on t.id = c.teacher_id left join student st on st.id = enroll.student_id
+where if(:subject_id = 0, true, c.subject_id = :subject_id) and c.subject_id = 1
